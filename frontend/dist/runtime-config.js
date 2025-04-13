ca0b4807f92df6cf662281012fb81883c3509917
twindai <guandai@hotmail.com>
2025-04-13 11:10:02 +0200

    update

---
+++ frontend/dist/runtime-config.js
@@ -0,0 +1,9 @@
+// public/runtime-config.template.js
+window.RUNTIME_CONFIG = {
+  REACT_APP_BE_URL: 'https://dev.twindai.com/api',
+  REACT_APP_FE_URL: 'https://dev.twindai.com/',
+  REACT_APP_BEANS_API_URL: 'https://isp.beans.ai/enterprise/v1/lists',
+  REACT_APP_BEANS_API_KEY: 'Basic MjAxNThkMTMzNjlkNGU4OjMxNjEzMTMxNjM2MzYzNjUzNjM2NjE2NDM0MzQ2NTM1MzgzMjM1Mzg',
+  REACT_APP_SOCKET_IO_HOST: 'https://dev.twindai.com/',
+  REACT_APP_SOCKET_IO_PORT: ''
+};
