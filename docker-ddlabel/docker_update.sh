caed6a2c056a48ef24057eeefffa8a4274520c89
twindai <guandai@hotmail.com>
2025-04-21 22:32:36 +0200

    add update

---
+++ docker-ddlabel/docker_update.sh
@@ -0,0 +1,3 @@
+docker pull guandai/ddlabel-backend:latest
+docker pull guandai/ddlabel-frontend:latest
+docker pull mysql:8.0
---
+++ docker-ddlabel/update_env.sh
@@ -0,0 +1,2 @@
+scp ../backend/.env.production root@ssh.twindai.com:/ddlabel/backend/.env.production
+scp ../frontend/.env.production root@ssh.twindai.com:/ddlabel/frontend/.env.production
